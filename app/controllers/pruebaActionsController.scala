e1fb73fda9124bf1ea52df1c824a4c67442e29df
andres.osorio <andres.osorio@ceiba.com.co>
2018-05-23 15:42:46 -0500

    se agregaron otros routes y otra vista

--- app/controllers/pruebaActionsController.scala
+++ app/controllers/pruebaActionsController.scala
@@ -4,7 +4,13 @@ import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 class pruebaActionsController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+
   def pruebaActionRequest = Action{
     implicit request => Ok("Got Request con Implicit ["+request +"]")
   }
+
+  def actionJSON = Action(parse.json) {
+    implicit request =>
+      Ok("Got request [" + request + "]")
+    }
 }
